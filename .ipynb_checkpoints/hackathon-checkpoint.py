c41dbc4b6d414ee788fc098036c8222c8598ac54
Pierre-Guillaume Petitmangin <pierre-guillaume.petitmangin@etu.minesparis.psl.eu>
2024-10-29 17:13:39 +0100

    ajout PG

--- .ipynb_checkpoints/hackathon-checkpoint.py
+++ .ipynb_checkpoints/hackathon-checkpoint.py
@@ -1,4 +1,7 @@
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # +
 # L'objet de cette partie est de trouver les pays qui ont eu les plus grosses explosions en terme d'importation / exportation depuis 1980,
@@ -46,8 +49,8 @@ plt.legend()
 plt.title('Importation du top 3 entre 1980 et 2021')
 plt.show()
 
-# +
-#on fait exactement le même travail, mais cette fois-ci sur l'exportation
+
+# On fait exactement le même travail, mais cette fois-ci sur l'exportation.
 
 df = pd.read_csv("world-country-electricity.csv", na_values=("--", "ie"))
 
@@ -81,6 +84,156 @@ plt.plot(X,Y3, label='Thaïlande')
 plt.legend()
 plt.title('Exportation du top 3 entre 1980 et 2021')
 plt.show()
-# -
+
+## Le but de cette partie est de s'intéresser à la consommation totale et la prodution totale par année de chaque continent en kwH 
+df = pd.read_csv('world-country-electricity.csv',na_values=("--","ie"))
+
+df_cons = df[df['Features'] == 'net consumption']
+df_gen = df[df['Features'] == 'net generation']
+
+Africa_cons = df_cons[df_cons['Region'] == 'Africa']
+Africa_gen = df_gen[df_gen['Region'] == 'Africa']
+
+Eurasia_cons = df_cons[df_cons['Region'] == 'Eurasia']
+Eurasia_gen = df_gen[df_gen['Region'] == 'Eurasia']
+
+Europe_cons = df_cons[df_cons['Region'] == 'Europe']
+Europe_gen = df_gen[df_gen['Region'] == 'Europe']
+
+Asia_Oceania_cons = df_cons[df_cons['Region'] == 'Asia & Oceania']
+Asia_Oceania_gen = df_gen[df_gen['Region'] == 'Asia & Oceania']
+
+Middle_East_cons = df_cons[df_cons['Region'] == 'Middle East']
+Middle_East_gen = df_gen[df_gen['Region'] == 'Middle East']
+
+North_America_cons = df_cons[df_cons['Region'] == 'North America']
+North_America_gen = df_gen[df_gen['Region'] == 'North America']
+
+Central_South_America_cons = df_cons[df_cons['Region'] == 'Central & South America']
+Central_South_America_gen = df_gen[df_gen['Region'] == 'Central & South America']
 
 
+
+L = []
+for i in range (1980,2022,1) : 
+    L.append(str(i))
+
+
+Africa_cons_ok = Africa_cons[L]
+
+Af_cons_sommes_par_annee = Africa_cons_ok.sum(axis=0)
+
+Af_cons_sommes = Af_cons_sommes_par_annee.values.tolist()
+X = np.arange(1980,2022,1)
+plt.plot(X,Af_cons_sommes,label ='Afrique')
+
+plt.xlabel("Année")
+plt.ylabel("Somme")
+plt.title("Somme des valeurs de consommation par année")
+plt.yticks([])
+plt.xticks([1980,1988, 1993, 1998, 2003, 2008, 2013, 2018, 2021])
+
+
+EUA_cons_ok = Eurasia_cons[L]
+
+EUA_cons_sommes_par_annee = EUA_cons_ok.sum(axis=0)
+EUA_cons_sommes = EUA_cons_sommes_par_annee.values.tolist()
+
+plt.plot(X,EUA_cons_sommes, label ='Eurasia')
+
+
+EU_cons_ok = Europe_cons[L]
+
+EU_cons_sommes_par_annee = EU_cons_ok.sum(axis=0)
+EU_cons_sommes = EU_cons_sommes_par_annee.values.tolist()
+
+plt.plot(X,EU_cons_sommes,label ='Europe')
+
+AO_cons_ok = Asia_Oceania_cons[L]
+
+AO_cons_sommes_par_annee = AO_cons_ok.sum(axis=0)
+AO_cons_sommes = AO_cons_sommes_par_annee.values.tolist()
+
+#plt.plot(X,AO_cons_sommes,label ='Asie et Oceanie') l'Asie tasse les autres variations donc on choisit ou non de le plot
+
+ME_cons_ok = Middle_East_cons[L]
+ME_cons_sommes_par_annee = ME_cons_ok.sum(axis=0)
+ME_cons_sommes = ME_cons_sommes_par_annee.values.tolist()
+
+plt.plot(X,ME_cons_sommes,label='Middle East')
+
+
+NO_cons_ok = North_America_cons[L]
+NO_cons_sommes_par_annee = NO_cons_ok.sum(axis=0)
+NO_cons_sommes = NO_cons_sommes_par_annee.values.tolist()
+
+plt.plot(X,NO_cons_sommes,label = 'North America')
+
+CS_cons_ok = Central_South_America_cons[L]
+CS_cons_sommes_par_annee = CS_cons_ok.sum(axis=0)
+CS_cons_sommes = CS_cons_sommes_par_annee.values.tolist()
+
+plt.plot(X,CS_cons_sommes,label='Central South America')
+plt.legend()
+plt.show()
+
+
+Africa_gen_ok = Africa_gen[L]
+
+Af_gen_sommes_par_annee = Africa_gen_ok.sum(axis=0)
+
+Af_gen_sommes = Af_gen_sommes_par_annee.values.tolist()
+X = np.arange(1980,2022,1)
+plt.plot(X,Af_gen_sommes,label ='Afrique')
+
+plt.xlabel("Année")
+plt.ylabel("Somme")
+plt.title("Somme des valeurs de production par année")
+plt.yticks([])
+plt.xticks([1980,1988, 1993, 1998, 2003, 2008, 2013, 2018, 2021])
+
+
+EUA_gen_ok = Eurasia_gen[L]
+
+EUA_gen_sommes_par_annee = EUA_gen_ok.sum(axis=0)
+EUA_gen_sommes = EUA_gen_sommes_par_annee.values.tolist()
+
+plt.plot(X,EUA_gen_sommes, label ='Eurasia')
+
+
+EU_gen_ok = Europe_gen[L]
+
+EU_gen_sommes_par_annee = EU_gen_ok.sum(axis=0)
+EU_gen_sommes = EU_gen_sommes_par_annee.values.tolist()
+
+plt.plot(X,EU_gen_sommes,label ='Europe')
+
+AO_gen_ok = Asia_Oceania_gen[L]
+
+AO_gen_sommes_par_annee = AO_gen_ok.sum(axis=0)
+AO_gen_sommes = AO_gen_sommes_par_annee.values.tolist()
+
+#plt.plot(X,AO_gen_sommes,label ='Asie et Oceanie') l'Asie tasse les autres variations donc on choisit ou non de le plot
+
+ME_gen_ok = Middle_East_gen[L]
+ME_gen_sommes_par_annee = ME_gen_ok.sum(axis=0)
+ME_gen_sommes = ME_gen_sommes_par_annee.values.tolist()
+
+plt.plot(X,ME_gen_sommes,label='Middle East')
+
+
+NO_gen_ok = North_America_gen[L]
+NO_gen_sommes_par_annee = NO_gen_ok.sum(axis=0)
+NO_gen_sommes = NO_gen_sommes_par_annee.values.tolist()
+
+plt.plot(X,NO_gen_sommes,label = 'North America')
+
+CS_gen_ok = Central_South_America_gen[L]
+CS_gen_sommes_par_annee = CS_gen_ok.sum(axis=0)
+CS_gen_sommes = CS_gen_sommes_par_annee.values.tolist()
+
+plt.plot(X,CS_gen_sommes,label='Central South America')
+plt.legend()
+plt.show()
+
+### fin de la partie 
